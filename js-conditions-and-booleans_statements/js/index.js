e55c2400892964964a9505b0dd9657e25ab456ac
Daniel Greßmann <daniel@gressmann.online>
2025-02-13 12:38:56 +0100

    Coach heißt endlich Coach

--- js-conditions-and-booleans_statements/js/index.js
+++ js-conditions-and-booleans_statements/js/index.js
@@ -33,15 +33,17 @@ if (numberOfHotdogs < 5) {
 }
 
 // Part 4: Daytime
-const currentHour = 12;
+const currentHour = 17;
 
-const statement = "";
+const statement = currentHour < 17 ? "Still need to learn." : "Partytime!!!";
 
 console.log(statement);
 
 // Part 5: Greeting
-const userName = "Archibald";
+const userName = "Daniel";
 
-const greeting = "Hello " + "//enter your code here" + "!";
+const coachName = "Archibald";
+
+const greeting = "Hello " + (userName === coachName ? "Coach" : userName + "!");
 
 console.log(greeting);
