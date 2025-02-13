49068cd91887b4952682a3f41825c5ee985cc038
Daniel Greßmann <daniel@gressmann.online>
2025-02-13 12:10:11 +0100

    improving at Part 3

--- js-conditions-and-booleans_statements/js/index.js
+++ js-conditions-and-booleans_statements/js/index.js
@@ -5,11 +5,31 @@ const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
 const receivedPassword = "password1234";
 
+if (receivedPassword === SUPER_SECRET_PASSWORD) {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} else {
+  console.log("Access denied");
+}
 // Part 2: Even / Odd
 const number = 6;
 
+if (number % 2 === 0) {
+  console.log("even number");
+} else {
+  console.log("odd number");
+}
+
 // Part 3: Hotdogs
-const numberOfHotdogs = 42;
+const numberOfHotdogs = 102;
+
+if (numberOfHotdogs < 5) {
+  console.log("2 Euro pro Hotdog");
+} else if (numberOfHotdogs > 4 && numberOfHotdogs < 100) {
+  console.log("1.50 Euro pro Hotdog");
+} else if (numberOfHotdogs < 100 && numberOfHotdogs < 1000000) {
+  console.log("1 Euro pro Hotdog");
+} else {
+}
 
 // Part 4: Daytime
 const currentHour = 12;
