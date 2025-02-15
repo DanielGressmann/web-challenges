2fe2c2cd1e6783b495d270906a9c7bc10b8b1071
Daniel Greßmann <daniel@gressmann.online>
2025-02-15 20:20:27 +0100

    initial commit

--- js-basics/js-basics-and-variables_lifetime-calculator/index.js
+++ js-basics/js-basics-and-variables_lifetime-calculator/index.js
@@ -20,3 +20,10 @@ Hint: You can combine text and variables using the `+` operator.
 const currentAge = Number(process.argv[2]); // This takes the `<age>` entered in the command `node index.js <age>`.
 
 console.log("Your current age is: " + currentAge);
+
+const livingDays = currentAge * 365;
+
+const lifeExpectancy = 80;
+const remainingDays = (lifeExpectancy - currentAge) * 365;
+
+const percentageLived = ((currentAge / lifeExpectancy) * 100).toFixed(2);
