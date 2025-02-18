a3fead9fe1fc3c3af3d44b532e12090160e066cd
Daniel Greßmann <daniel@gressmann.online>
2025-02-19 00:27:14 +0100

    melli solution

--- js-create-element/js-createelement_social-media-post/js/index.js
+++ js-create-element/js-createelement_social-media-post/js/index.js
@@ -8,6 +8,16 @@ function handleLikeButtonClick(event) {
 const likeButton = document.querySelector('[data-js="like-button"]');
 likeButton.addEventListener("click", handleLikeButtonClick);
 
+const h1 = document.querySelector("h1");
+const h1Clone = h1.cloneNode(true);
+const article = document.querySelector("article");
+const post = document.querySelector("post");
+const footer = document.querySelector("footer");
+const button = document.querySelector("button");
+
+console.log(`h1: ${h1.textContent} und article ${article}`);
+console.log("article: ", article);
+
+document.body.appendChild(h1Clone);
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
-
