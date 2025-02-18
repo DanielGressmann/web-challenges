86761dc95f61af0860a85288157bd158a607bea8
Daniel Greßmann <daniel@gressmann.online>
2025-02-18 12:06:06 +0100

    work on bonus left

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,23 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+
+  event.target.reset();
+});
+
+// const age = Number(data.age) || 0;
+// const badness = Number(data.badness) || 0;
+// const ageBadnessSum = age + badness;
+
+//console.log(`The age-badness-sum of "${data.firstName}" is "${ageBadnessSum}"`);
+
+//form.reset();
+//firstNameInput.focus();
