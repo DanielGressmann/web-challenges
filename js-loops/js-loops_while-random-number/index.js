8bc9fff1fb1cd61d2a72105d17566e8b43f4302c
Daniel Greßmann <daniel@gressmann.online>
2025-02-20 12:43:58 +0100

    easy done

--- js-loops/js-loops_while-random-number/index.js
+++ js-loops/js-loops_while-random-number/index.js
@@ -4,7 +4,12 @@ let number = 0;
 let count = 0;
 
 // --v-- write or modify code below this line --v--
-
+while (number <= 0.9) {
+  // solange die Zahl unter 0.9 ist…
+  number = Math.random(); // generiere eine neue Zufallszahl…
+  console.log(number); // (Zeige die Zahl)
+  count++; // … und erhöhe den Zähler
+}
 // --^-- write or modify code above this line --^--
 
 console.log(
