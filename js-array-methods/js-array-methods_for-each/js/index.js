571048950897d90590337a00dc897a65c9034ca4
Daniel Greßmann <daniel@gressmann.online>
2025-02-21 12:19:11 +0100

    esolve Task 1+2 mit forEach und einer Funktion

--- js-array-methods/js-array-methods_for-each/js/index.js
+++ js-array-methods/js-array-methods_for-each/js/index.js
@@ -19,4 +19,34 @@ const colors = [
   "#A5A5A5",
 ];
 
-colors.forEach();
+// TASK 1
+colors.forEach((color) => {
+  // 1a. Neues div-Element erstellen
+  const coloredBox = document.createElement("div");
+
+  // 1b. die Klasse color-box hinzufügen
+  coloredBox.classList.add("color-box");
+
+  // 2. Die Hintergrundfarbe setzen
+  coloredBox.style.backgroundColor = color;
+
+  // 3. coloredBox-Element zum Body hinzufügen
+  document.body.append(coloredBox);
+
+  // 3.1 Altenative:
+  // Erst const body = document.body; definieren,
+  // dann geht auch body.append(coloredBox);
+});
+
+// TASK 2
+
+// 1. Funktion zum erstellen einer farbigen Box
+function renderColorBox(color) {
+  const coloredBox = document.createElement("div");
+  coloredBox.classList.add("color-box");
+  coloredBox.style.backgroundColor = color;
+  document.body.append(coloredBox);
+}
+
+// forEach mit Funktionsreferenz benutzen - ist einfacher als Arrow-Funktion ((color) => {})
+colors.forEach(renderColorBox);
