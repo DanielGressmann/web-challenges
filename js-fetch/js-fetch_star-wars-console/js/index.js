26eb9ccccb1371a32630f1992717655f3812e4af
Daniel Greßmann <daniel@gressmann.online>
2025-02-24 11:09:46 +0100

    play around

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,11 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const data = await fetch(url); // Daten abrufen
+  const dataObject = await data.json(); // In JSON umwandeln
+
+  console.log("All Data", dataObject.results[2].eye_color);
+}
 
 fetchData();
