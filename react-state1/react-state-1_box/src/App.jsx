e525b5926c5dd65d2863287746d8dd49c172e34c
Daniel Greßmann <daniel@gressmann.online>
2025-03-03 16:42:39 +0100

    solve bonus task by using ternery operator and hacker bonus: asynchrones lesen der Updates für bessere Perrformance

--- react-state1/react-state-1_box/src/App.jsx
+++ react-state1/react-state-1_box/src/App.jsx
@@ -1,18 +1,23 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let isActive = false;
+  // let isActive = false; --> ersetzten durch useState:
+  const [isActive, setIsActive] = useState(false);
 
   function handleClick() {
-    isActive = !isActive;
-    // Check that the value changes correctly.
-    console.log(isActive);
+    // isActive = !isActive;
+    // // Check that the value changes correctly.
+    // console.log(isActive); --> State-Update mit setIsActive:
+    setIsActive(!isActive);
   }
-
+  console.log(isActive); // HACKER BONUS --> State-Updates werden asynchron eingelesen. Die Veränderung wird erst beim neuen Rendern angezeigt - bessere Performance?
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
-    </main>
+      <button onClick={handleClick}>
+        {isActive ? "Deaktivieren!" : "Aktivieren!"}
+      </button>
+    </main> //ternery nutzen für BONUS
   );
 }
