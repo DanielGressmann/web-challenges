fc2a5b830028f55a7666a780a653d7aee4cec51a
Daniel Greßmann <daniel@gressmann.online>
2025-03-04 15:35:37 +0100

    solve task one and bonus

--- react-arrays/react-with-arrays_fruits/src/components/Card.jsx
+++ react-arrays/react-with-arrays_fruits/src/components/Card.jsx
@@ -1,5 +1,10 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+export default function Card({ name, color }) {
+  return (
+    <p className="card" style={{ backgroundColor: color }}>
+      {name}
+    </p>
+  ); //--> Option A mit inline JS
+  // return <p className={`card ${color}`}>{name}</p>; --> Option B - hierbei mit müssten noch für jede Farbe eine eigene CSS Klasse hinzugefügt werden
 }
