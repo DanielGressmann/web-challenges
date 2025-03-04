1991084beb01b58f3a725b6224e307306ba108ce
Daniel Greßmann <daniel@gressmann.online>
2025-03-04 15:59:54 +0100

    solve first task and create dynamic counterfor colored numbers

--- react-state2/react-state-2_colored-number/src/components/Counter.jsx
+++ react-state2/react-state-2_colored-number/src/components/Counter.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react"; // useState importieren
 import ColoredNumber from "./ColoredNumber";
 
 export default function Counter() {
+  const [count, setCount] = useState(0); // State für den Counter
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      {/* {count als value übergeben} */}
+      {/* <ColoredNumber value={0} /> --> */} <ColoredNumber value={count} />
       <div className="counter__buttons-container">
         <button
           type="button"
           className="counter__button"
           aria-label="increment count"
+          onClick={() => setCount(count + 1)} // Counter um 1 erhöhen
         >
           +
         </button>
@@ -17,6 +21,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement count"
+          onClick={() => setCount(count - 1)} // Counter um 1 verringern
         >
           -
         </button>
