98c76a2abe187a5687afcf19c34899c2e3694e6e
Daniel Greßmann <daniel@gressmann.online>
2025-03-10 14:55:10 +0100

    initial and final commit

---
+++ react-state1/react-state-1_counter/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <div className="button-container">
+        <button
+          type="button"
+          onClick={() => setCount((count) => count - 1)} // Zählt runter
+        >
+          -
+        </button>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
