4719c1b9de068fa4133d2f218cee870585252e59
Daniel Greßmann <daniel@gressmann.online>
2025-03-15 17:41:47 +0100

    ongoing

--- js-unit-testing/react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
+++ js-unit-testing/react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Navigation from ".";
+import { useRouter } from "next/router";
 
 jest.mock("next/router", () => ({
   useRouter() {
@@ -7,4 +8,24 @@ jest.mock("next/router", () => ({
   },
 }));
 
-test("renders with two links 'Play' and 'History'", () => {});
+// test("renders with two links 'Play' and 'History'", () => {
+//   render(<Navigation player={[]} />);
+
+//   const playLink = screen.getByRole("link", { name: /play/i });
+//   const historyLink = screen.getByRole("Link", { name: /history/i });
+
+//   expect(playLink).toBeInTheDocument();
+//   expect(historyLink).toBeInTheDocument();
+// }); // Chat GPT Lösung
+
+test("renders with two links 'Play' and 'History'", () => {
+  render(<Navigation />);
+
+  const playLink = screen.getByRole("Link", { name: "Play" });
+  expect(playLink).toBeInTheDocument();
+
+  const historyLink = screen.getByRole("Link", {
+    name: "History",
+  });
+  expect(historyLink).toBeInTheDocument();
+});
