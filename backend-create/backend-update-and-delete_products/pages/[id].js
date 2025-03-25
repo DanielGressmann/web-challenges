57b0ff38c396f792400e49f8e5d9586051ce9480
Daniel Greßmann <daniel@gressmann.online>
2025-03-25 20:51:49 +0100

    implement buttons for deleting, adding and editing fish and save it in the data bank

--- backend-create/backend-update-and-delete_products/pages/[id].js
+++ backend-create/backend-update-and-delete_products/pages/[id].js
@@ -1,30 +1,96 @@
+import { useState } from "react";
 import useSWR from "swr";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import StyledLink from "@/components/Link";
+import ProductForm from "@/components/ProductForm";
 
 export default function Product() {
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const {
+    data: product,
+    isLoading,
+    mutate,
+  } = useSWR(id ? `/api/products/${id}` : null);
+
+  const [isEditMode, setIsEditMode] = useState(false);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
-  if (!data) {
+  if (!product) {
     return;
+  }
+
+  // Funtion zum Bearbeiten des Produktes
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      console.error("Update failed", response.status);
+      return;
+    }
+
+    mutate(); // Aktualisierte Daten abrufen
+    setIsEditMode(false); // Bearbeitungsmodus beendet
+  }
+
+  // DELETE Product Function
+
+  async function handleDeleteProduct() {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      console.error("Delete failed", response.status);
+      return;
+    }
+    router.push("/");
   }
 
   return (
     <ProductCard>
-      <h2>{data.name}</h2>
-      <p>Description: {data.description}</p>
-      <p>
-        Price: {data.price} {data.currency}
-      </p>
-      <StyledLink href="/">Back to all</StyledLink>
+      <button type="button" onClick={() => setIsEditMode(!isEditMode)}>
+        {isEditMode ? "Cancel Edit" : "Edit Product"}
+      </button>
+
+      {isEditMode && (
+        <>
+          <h2>Edit Product</h2>
+          <ProductForm
+            onSubmit={handleEditProduct}
+            title="Edit Product"
+            defaultValues={product}
+          />
+        </>
+      )}
+
+      {!isEditMode && (
+        <>
+          <h2>{product.name}</h2>
+          <p>Description: {product.description}</p>
+          <p>
+            Price: {product.price} {product.currency}
+          </p>
+          <StyledLink href="/">Back to all</StyledLink>
+          <button type="button" onClick={() => handleDeleteProduct(id)}>
+            Delete Button
+          </button>
+        </>
+      )}
     </ProductCard>
   );
 }
--- backend-create/backend-update-and-delete_products/pages/api/products/[id].js
+++ backend-create/backend-update-and-delete_products/pages/api/products/[id].js
@@ -17,5 +17,28 @@ export default async function handler(request, response) {
     return;
   }
 
+  if (request.method === "PUT") {
+    const updatedProduct = request.body;
+    const product = await Product.findByIdAndUpdate(id, updatedProduct);
+
+    if (!product) {
+      response.status(404).json({ status: "Not Found" });
+      return;
+    }
+    response.status(200).json({ status: "Product successfully updated" });
+  }
+
+  if (request.method === "DELETE") {
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      response.status(404).json({ status: "Not Found" });
+      return;
+    }
+
+    response.status(200).json({ status: "Product successfully deleted." });
+    return;
+  }
+
   response.status(405).json({ status: "Method not allowed." });
 }
